2f5161376907a85ac5812bda942515d76728a927
GonzaGomezPizarro <1912988@ucc.edu.ar>
2024-08-22 18:01:19 -0300

    TEST

--- src/components/NewUser.jsx
+++ src/components/NewUser.jsx
@@ -1,35 +1,56 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import {toast} from "sonner"
 
 const NewUser = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
 
     const createUser = async () => {
+        if (username === "") {
+            setError("Insert a username!")
+            toast.error("Insert a username!")
+            return
+        } else if (password === "") {
+            setError("Insert a password!")
+            toast.error("Insert a password!")
+            return
+        } else {
+            setError(null)
+        }
         try {
             const response = await axios.post('https://backend-lxkjskx52a-uc.a.run.app/user', {
                 username,
                 password
             });
             console.log(response.data); // Handle the response data as needed
+            if (response.data.UserId){ // 
+                toast.info("User created successfully")
+            }
         } catch (error) {
             console.error(error); // Handle any errors that occur during the request
+            if (error?.response?.data?.detail?.includes("Duplicate entry")) {
+                toast.info(`The username '${username}' is already taken!`);
+            } else {
+                toast.info("An error occurred. Please try again.");
+            }
         }
     };
 
     return (
         <div className="grid justify-center mt-10">
             <p className="text-3xl mb-5 text-center font-medium">{"Create a user here =)"}</p>
-            <div className="mb-5 bg-red-900">
+            <div className="mb-5 ">
                 <label className="block text-gray-400">Username</label>
-                <input className="w-full" type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input className="w-full bg-gray-950 h-10 rounded-md px-4 mb-4" type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
                 <label className="block text-gray-400">Password</label>
-                <input className="w-full" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input className="w-full bg-gray-950 h-10 rounded-md px-4" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
             </div>
             <div className="flex justify-center">
-
-            <button className="bg-gray-950 px-3 py-2 rounded-xl hover:scale-[106%] transform transition-transform" onClick={createUser}>Create user</button>
+                <button className="bg-gray-950 px-3 py-2 rounded-xl hover:scale-[106%] transform transition-transform" onClick={createUser}>Create user</button>
             </div>
+            {error && (<p className="text-orange-800 text-center mt-2"><i className="bi bi-exclamation-triangle-fill"></i>{"  "}{error}</p>)}
         </div>
     );
 };
