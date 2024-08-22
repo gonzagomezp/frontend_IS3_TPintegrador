f02b039abafd45a9473ff75b5e10265acba163c3
GonzaGomezPizarro <1912988@ucc.edu.ar>
2024-08-22 17:17:58 -0300

    test

--- src/components/NewUser.jsx
+++ src/components/NewUser.jsx
@@ -18,10 +18,18 @@ const NewUser = () => {
     };
 
     return (
-        <div>
-            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={createUser}>Create User</button>
+        <div className="grid justify-center mt-10">
+            <p className="text-3xl mb-5 text-center font-medium">{"Create a user here =)"}</p>
+            <div className="mb-5 bg-red-900">
+                <label className="block text-gray-400">Username</label>
+                <input className="w-full" type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <label className="block text-gray-400">Password</label>
+                <input className="w-full" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <div className="flex justify-center">
+
+            <button className="bg-gray-950 px-3 py-2 rounded-xl hover:scale-[106%] transform transition-transform" onClick={createUser}>Create user</button>
+            </div>
         </div>
     );
 };
