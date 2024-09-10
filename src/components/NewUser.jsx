e87bef73d8612f5752461a64e42338366d9ce3c9
GonzaGomezPizarro <1912988@ucc.edu.ar>
2024-09-10 18:06:09 -0300

    ADD - loader to newUser.jsx

--- src/components/NewUser.jsx
+++ src/components/NewUser.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import {toast} from "sonner"
+import { toast } from "sonner"
+import Loader from './ui/Loader';
 
 const NewUser = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     const createUser = async () => {
         if (username === "") {
@@ -16,16 +18,16 @@ const NewUser = () => {
             setError("Insert a password!")
             toast.error("Insert a password!")
             return
-        } else {
-            setError(null)
         }
+        setError(null)
+        setIsLoading(true)
         try {
             const response = await axios.post('https://backend-lxkjskx52a-uc.a.run.app/user', {
                 username,
                 password
             });
             console.log(response.data); // Handle the response data as needed
-            if (response.data.UserId){ // 
+            if (response.data.UserId) { // 
                 toast.info("User created successfully")
                 setUsername('')
                 setPassword('')
@@ -39,6 +41,8 @@ const NewUser = () => {
                 toast.info(error.response.data.detail);
             }
         }
+        await new Promise(resolve => setTimeout(resolve, 700));
+        setIsLoading(false)
     };
 
     return (
@@ -51,7 +55,9 @@ const NewUser = () => {
                 <input className="w-full bg-gray-950 h-10 rounded-md px-4" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
             </div>
             <div className="flex justify-center">
-                <button className="bg-gray-950 px-3 py-2 rounded-xl hover:scale-[106%] transform transition-transform" onClick={createUser}>Create user</button>
+                {isLoading ? <Loader /> :
+                    <button className="bg-gray-950 px-3 py-2 rounded-xl hover:scale-[106%] transform transition-transform" onClick={createUser}>Create user</button>
+                }
             </div>
             {error && (<p className="text-orange-800 text-center mt-2"><i className="bi bi-exclamation-triangle-fill"></i>{"  "}{error}</p>)}
         </div>
